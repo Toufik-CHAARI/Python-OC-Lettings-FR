697495b0b6ae5b830d1e229e52975581deff35c8
Toufik-CHAARI <Toufik-CHAARI>
2024-01-21 15:22:47 +0100

    application splitted in 2 apps,old tables are deleted andata migration is successful

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chaaritoufik/Desktop/dev/OPENCLASSROOM/PROJET13/Python-OC-Lettings-FR/env/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ profiles/migrations/0002_auto_20240121_1355.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+def transfer_data(apps, schema_editor):
+    OldProfile = apps.get_model('oc_lettings_site', 'Profile')
+    NewProfile = apps.get_model('profiles', 'Profile')
+
+    for old_profile in OldProfile.objects.all():
+        NewProfile.objects.create(
+            user=old_profile.user,
+            favorite_city=old_profile.favorite_city
+        )
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('profiles', '0001_initial'),
+        ('oc_lettings_site', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(transfer_data),
+    ]
