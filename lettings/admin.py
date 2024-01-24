7d2152eacbb404d732dfdcf96aa7bd3b1d78f256
Toufik-CHAARI <Toufik-CHAARI>
2024-01-24 19:15:57 +0100

    doctring and flake8 reports are clean/ no error part:2

--- lettings/admin.py
+++ lettings/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import Letting
 from .models import Address
 
-
+# Register the Letting and Address models in the Django admin site.
 
 admin.site.register(Letting)
 admin.site.register(Address)
-
--- lettings/models.py
+++ lettings/models.py
@@ -2,8 +2,20 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
-
 class Address(models.Model):
+    """
+    Represents an address in the database.
+    Attributes:
+        number (PositiveIntegerField): The street number of the address, must be less than 9999.
+        street (CharField): The name of the street, up to 64 characters.
+        city (CharField): The city name, up to 64 characters.
+        state (CharField): The state code, exactly 2 characters.
+        zip_code (PositiveIntegerField): The zip code, must be less than 99999.
+        country_iso_code (CharField): The country ISO code, exactly 3 characters.
+
+    Methods:
+        __str__: Returns a human-readable string representation of the address.
+    """
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -12,17 +24,23 @@ class Address(models.Model):
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     def __str__(self):
-        return f'{self.number} {self.street}'
+        return f"{self.number} {self.street}"
+
     class Meta:
         verbose_name_plural = "Address"
 
 
 class Letting(models.Model):
+    """
+    Represents a letting in the database.
+    Attributes:
+        title (CharField): The title of the letting, up to 256 characters.
+        address (OneToOneField): A one-to-one relationship linking the letting to its address.
+    Methods:
+        __str__: Returns a human-readable string representation of the letting.
+    """
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
-
-
