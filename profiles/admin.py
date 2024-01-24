5cec803a37a1d290a37d17305deb97ec65c3dc28
Toufik-CHAARI <Toufik-CHAARI>
2024-01-24 19:10:50 +0100

    docstrings added and flake8 report is clean/no errors

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 from .models import Profile
 
-
+# Register the Profile model in the Django admin site.
 
 admin.site.register(Profile)
--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 
 class Profile(models.Model):
+    """
+    Represents a user profile in the database.
+    Attributes:
+        user (OneToOneField): A one-to-one relationship linking the profile to a Django User
+        model instance.
+        favorite_city (CharField): The user's favorite city, up to 64 characters. This field
+        is optional.
+    Methods:
+        __str__: Returns a human-readable string representation of the profile, specifically
+        the associated user's username.
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
