1077ff956de7d59050be8b5646e168ba1762d353
Toufik-CHAARI <Toufik-CHAARI>
2024-02-16 13:17:44 +0100

    tag latest implemented for dynamically pulling the last version and run it locally

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 # Use the DOCKERHUB_USERNAME environment variable
 DOCKERHUB_USERNAME="${DOCKERHUB_USERNAME}"  # No need to replace; it uses the environment variable
 APP_NAME="oc_lettings_site"  # Replace with your Docker image name
-COMMIT_HASH="latest"  # Use "latest" or specify a commit hash
+TAG="latest"  # Use "latest" or specify a commit hash
 
 # Check if DOCKERHUB_USERNAME is set
 if [ -z "$DOCKERHUB_USERNAME" ]; then
@@ -13,8 +13,10 @@ fi
 
 # Pull the Docker image from Docker Hub
 echo "Pulling the Docker image..."
-docker pull $DOCKERHUB_USERNAME/$APP_NAME:$COMMIT_HASH
+docker pull $DOCKERHUB_USERNAME/$APP_NAME:$TAG
+
+export PORT=8000
 
 # Run the Docker container
 echo "Running the Docker container..."
-docker run -p 8000:8000 $DOCKERHUB_USERNAME/$APP_NAME:$COMMIT_HASH
+docker run -e PORT=8000 -p 8000:8000 $DOCKERHUB_USERNAME/$APP_NAME:$TAG
